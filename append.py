8be8770ce3b285e22178351ac88f0c444ce4bb45
VIDHI SEN <100829319+vidhisen4699@users.noreply.github.com>
2022-03-04 10:55:46 +0530

    Add files via upload

---
+++ append.py
@@ -0,0 +1,6 @@
+# a=['ram','sita','meeta','riya']
+# a.append('Vidhi')
+# print(a)
+
+
+print(26)
---
+++ cmp.py
@@ -0,0 +1,11 @@
+list1=[2,4,6,8]
+list2=[2,4,6,9]
+list3=[2,4,6,9,11]
+list4=[2,4,6,8]
+
+# print("Comparing list1 and list2:")
+# print(cmp(list1,list2))
+# print("comparing list2 and list3:")
+# print(cmp(list2,list3))
+# print("comparing list1 and list4")
+# print(cmp(list1,list4))
